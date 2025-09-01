97ea2c687090edd5561f278a7796492a2e276076
Mjeed101 <Mjeed102@proton.me>
2025-09-02 01:20:27 +0300

    aa

--- config/services.php
+++ config/services.php
@@ -35,10 +35,10 @@ return [
         ],
     ],
 
-    'google' => [
-    'client_id' => env('GOOGLE_CLIENT_ID'),
+     'google' => [
+    'client_id'     => env('GOOGLE_CLIENT_ID'),
     'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-    'redirect' => env('GOOGLE_REDIRECT_URI', 'http://127.0.0.1:8000/auth/google/callback'),
+    'redirect'      => env('GOOGLE_REDIRECT_URI', config('app.url').'/auth/google/callback'),
 ],
 
 
